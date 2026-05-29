1f4c5a8944e37c1afbc7cbc28e304a584603b176
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-05-29 15:15:47 +1000

    created uniqueIDCheck for driver repository test

---
+++ a4/src/test/java/DriverRepositoryTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class DriverRepositoryTest {
+    
+    @Test
+    void driverIDUniqueCheck(){
+        var dr = new DriverRepository();
+        String id = "33$$1111AA";
+        String name = "John Doe";
+        int experienceYears = 1;
+        String licenseType = "Light";
+        String address = "1|Sesame St.|Melbourne|Victoria|Australia";
+        String birthdate = "01-01-2000";
+
+        dr.Add(id, name, experienceYears, licenseType, address, birthdate);
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                dr.Add(id, name, experienceYears, licenseType, address, birthdate);
+            });
+    }
+
+}
