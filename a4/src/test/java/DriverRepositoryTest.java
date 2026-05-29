e05113f28fdcd93925adae4f45a44fa229b3d314
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-05-29 15:20:25 +1000

    added test for driverIDs with >10 characters

--- a4/src/test/java/DriverRepositoryTest.java
+++ a4/src/test/java/DriverRepositoryTest.java
@@ -21,4 +21,21 @@ public class DriverRepositoryTest {
             });
     }
 
+    @Test
+    void DriverIDLength11Fails(){
+        var dr = new DriverRepository();
+        String id = "33$$11111AA";
+        String name = "John Doe";
+        int experienceYears = 1;
+        String licenseType = "Light";
+        String address = "1|Sesame St.|Melbourne|Victoria|Australia";
+        String birthdate = "01-01-2000";
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                dr.Add(id, name, experienceYears, licenseType, address, birthdate);
+            });
+
+    }
+
 }
