3d14e80886716a70c36e3a41c425d3885a78b598
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-02 21:30:06 +1000

    added test case for driver birthday fail

--- a4/src/test/java/DriverRepositoryTest.java
+++ a4/src/test/java/DriverRepositoryTest.java
@@ -272,6 +272,22 @@ public class DriverRepositoryTest {
             });
     }
 
+    @Test
+    void DriverBirthdayWrongFormatFail(){
+        var dr = new DriverRepository();
+        String id = "33$$1111AA";
+        String name = "John Doe";
+        int experienceYears = 1;
+        String licenseType = "Light";
+        String address = "1|Sesame St.|Melbourne|Victoria|Australia";
+        String birthdate = "01/01/2000";
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                dr.Add(id, name, experienceYears, licenseType, address, birthdate);
+            });
+    }
+
 
    
 }
