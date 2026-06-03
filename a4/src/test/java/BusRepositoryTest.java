7533639c06911c328f71f56a55c55d6df4e431dc
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-03 15:09:53 +1000

    added test cases for valid bus id's

--- a4/src/test/java/BusRepositoryTest.java
+++ a4/src/test/java/BusRepositoryTest.java
@@ -38,4 +38,53 @@ public class BusRepositoryTest {
         assertDoesNotThrow(() -> {br.Add(new_id, capacity, fuelLevel, fuelType, driver);});
     }
 
+
+    @Test
+    public void BusIDLongerThanEightFail(){
+        BusRepository br = new BusRepository();
+        String id = "123456789";
+        int capacity = 30;
+        double fuelLevel = 27.8;
+        String fuelType = "Diesel";
+        Driver driver = new Driver("33$$1111AA", "John Doe", 1, "Light", 
+                            "1|Sesame St.|Melbourne|Victoria|Australia", "01-01-2000");
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                br.Add(id, capacity, fuelLevel, fuelType, driver);
+            });
+    }
+
+    @Test
+    public void BusIDShorterThanEightFail(){
+        BusRepository br = new BusRepository();
+        String id = "1234567";
+        int capacity = 30;
+        double fuelLevel = 27.8;
+        String fuelType = "Diesel";
+        Driver driver = new Driver("33$$1111AA", "John Doe", 1, "Light", 
+                            "1|Sesame St.|Melbourne|Victoria|Australia", "01-01-2000");
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                br.Add(id, capacity, fuelLevel, fuelType, driver);
+            });
+    }
+
+    @Test
+    public void BusIDContainsNonDigitFail(){
+        BusRepository br = new BusRepository();
+        String id = "1234A678";
+        int capacity = 30;
+        double fuelLevel = 27.8;
+        String fuelType = "Diesel";
+        Driver driver = new Driver("33$$1111AA", "John Doe", 1, "Light", 
+                            "1|Sesame St.|Melbourne|Victoria|Australia", "01-01-2000");
+
+        assertThrows(IllegalArgumentException.class, 
+            () -> {
+                br.Add(id, capacity, fuelLevel, fuelType, driver);
+            });
+    }
+
 }
