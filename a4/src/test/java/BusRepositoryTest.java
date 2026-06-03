233e49a0c3dbaafd792d2f12f390913379b48104
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-03 15:03:02 +1000

    added test case for checking that two unique id's can be added for busses

--- a4/src/test/java/BusRepositoryTest.java
+++ a4/src/test/java/BusRepositoryTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,22 @@ public class BusRepositoryTest {
             () -> {
                 br.Add(id, capacity, fuelLevel, fuelType, driver);
             });
+    }
+
+    @Test
+    public void BusIDUniquePass(){
+        BusRepository br = new BusRepository();
+        String id = "12345678";
+        int capacity = 30;
+        double fuelLevel = 27.8;
+        String fuelType = "Diesel";
+        Driver driver = new Driver("33$$1111AA", "John Doe", 1, "Light", 
+                            "1|Sesame St.|Melbourne|Victoria|Australia", "01-01-2000");
+
+        br.Add(id, capacity, fuelLevel, fuelType, driver);
+
+        String new_id = "87654321";
+        assertDoesNotThrow(() -> {br.Add(new_id, capacity, fuelLevel, fuelType, driver);});
     }
 
 }
