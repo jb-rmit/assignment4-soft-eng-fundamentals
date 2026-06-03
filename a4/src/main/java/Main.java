94fa46739ff8a837eb0d64d52740aadf9c510574
jb-rmit <s4165076@student.rmit.edu.au>
2026-06-03 11:52:34 +1000

    Skeleton file-reading code

--- a4/src/main/java/Main.java
+++ a4/src/main/java/Main.java
@@ -1,9 +1,24 @@
-
+import java.io.File;                  // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner;             // Import the Scanner class to read text files
 
 
 public class Main {
     static void main() {
 
+            File database = new File("bus_database.txt");
+
+
+            //code adapted from w3schools: https://www.w3schools.com/java/java_files_read.asp
+            // try-with-resources: Scanner will be closed automatically
+            try (Scanner myReader = new Scanner(database)) {
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File bus_database.txt not found.");
+            }
 
         }
     }
