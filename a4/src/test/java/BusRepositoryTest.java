09e1fe599cd874a407bd89d4aec06ba5aa43e463
AJ ROA <armandjohnroa@gmail.comgit config --global user.name AJ>
2026-06-04 13:41:56 +1000

    added test case for driver with less than 5 years of experience driving an electric bus

--- a4/src/test/java/BusRepositoryTest.java
+++ a4/src/test/java/BusRepositoryTest.java
@@ -201,6 +201,25 @@ public class BusRepositoryTest {
     }
 
     @Test
+    public void DriverUnderFiveYearsCannotDriveElectric(){
+        BusRepository br = new BusRepository();
+        String id = "12345678";
+        int capacity = 60;
+        double fuelLevel = 27.8;
+        String fuelType = "Electricity";
+        Driver driver = new Driver("33$$1111AA", "John Doe", 4, "Heavy", 
+                            "1|Sesame St.|Melbourne|Victoria|Australia", "01-01-2000");
+
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, 
+            () -> {
+                br.Add(id, capacity, fuelLevel, fuelType, driver);
+            });
+
+        System.out.println(exception);
+    }
+
+    @Test
     public void DriverLightCannotDriveHybrid(){
         BusRepository br = new BusRepository();
         String id = "12345678";
